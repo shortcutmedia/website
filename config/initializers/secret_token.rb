1c11d903fbd32ad53f8bc6be1e823d324252316d
herbertbay <herbert.bay@gmail.com>
2013-06-06 11:53:26 +0200

    deleted old files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Shortcutmedia::Application.config.secret_token = '0d5d7175df65e62b85f3ada1302142b089cf63065b076c269361681634d4983d4cbc6fccf8802e5ac68d2cfc8ac1c1e8ea7559ea7d998c43d24704d93c905f8b'
