c3942ec2c9e9616332b42cddcb1d341cb40cc73e
Franco Sebregondi <franco@nui.local>
2014-08-26 12:24:00 +0400

    spacing

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -9,17 +9,13 @@ module CustomHelpers
       .join(' ')
 
     concat_content(
-      [
-        div('&nbsp;', class: 'grid_1'),
-        div(class: 'grid_10') do
-          div(id: id, class: html_class) do
-            div(class: 'info-box-desc') do
-              div(inner_html, class: 'box-content')
-            end
+      div(class: 'grid_10 prefix_1 suffix_1') do
+        div(id: id, class: html_class) do
+          div(class: 'info-box-desc') do
+            div(inner_html, class: 'box-content')
           end
-        end,
-        content_tag(:div, '&nbsp;', class: 'grid_1')
-      ].join
+        end
+      end
     )
   end
 
