8c90cf901a2f10d4088789fb0459e039e1377aa6
Franco Sebregondi <me@usershell.org>
2014-09-23 11:25:34 +0200

    Include videos

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 //= require vendor/jquery-1.8.0.min
 //= require vendor/jquery-placeholder.js
+//= require vendor/modal
 //= require signup
 
 // Avoid `console` errors in browsers that lack a console.
@@ -15,12 +16,38 @@ if (!(window.console && console.log)) {
     }());
 }
 
+var youtubePlayers = {};
+function onYouTubeIframeAPIReady() {
+  $('.modal-youtube-video iframe').each( function() {
+    youtubePlayers[this.id] = new YT.Player(this.id);
+  });
+}
+
 $(function(){
+  var modalShow = function(e) {
+    var el = $(e.originalEvent.detail.modal);
+    player = youtubePlayers[ el.find('iframe').attr('id') ];
+    if (player) {
+      player.playVideo();
+    }
+  }
+  var modalHide = function(e) {
+    el = $(e.originalEvent.detail.modal);
+    player = youtubePlayers[ el.find('iframe').attr('id') ];
+    if (player) {
+      player.stopVideo();
+    }
+  }
+
   var ready = function(){
     // apply jquery placeholder which is a polyfill for IE8/9.
     $('input, textarea').placeholder();
   }
+
   $(document).ready(ready);
   $(document).on('page:load', ready);
   $(document).ajaxComplete(ready);
+
+  $(document).on('cssmodal:show', modalShow);
+  $(document).on('cssmodal:hide', modalHide);
 });
