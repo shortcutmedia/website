3c278b47976950d9e1b9da3829737fa539d3da1f
Herbert Bay <herbert.bay@gmail.com>
2015-06-11 08:58:24 -0400

    new website design

--- assets/js/shortcut.js
+++ assets/js/shortcut.js
@@ -68,17 +68,20 @@ $(document).ready(function() {
       }
      });
      
-    
-    
-     
-    // Tab Contact boxes     
+   $("#TabZuerich").on('shown.bs.tab', function() {
+     	/* Trigger map resize event */
+   	google.maps.event.trigger(mapZuerich, 'resize');
+    mapZuerich.setCenter(markerZuerich.getPosition());
+   });
+   
+   
    $(function () {
       //$('#contactTab a:last').tab('show');
       
-      var showMaps = function(showntab) {
-      	showntab.find('iframe').remove();      
-      	showntab.prepend('<iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="'+locations[showntab.attr('id')]+'"></iframe>');
-      
+      /* var showMaps = function(showntab) {
+      	//showntab.find('iframe').remove();      
+      	//showntab.prepend('<iframe id="my_map_canvas" width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="'+locations[showntab.attr('id')]+'"></iframe>');      
+        //showntab.find('iframe').addClass('scrollof');
       }
       
       if ($('.tab-pane').length) {
@@ -92,12 +95,10 @@ $(document).ready(function() {
        
        $('#contactTab a').on('shown', function (e) {
          var showntab = $($(e.target).attr("href")); // activated tab
-         showMaps(showntab);
-           
-         
+         showMaps(showntab);         
          //e.relatedTarget // previous tab
        })
-       
+       */
       
     })
     
@@ -114,22 +115,7 @@ $(document).ready(function() {
     	
     });
     
-    /** Scrolling stuff **/
     
-    
-    $('#home').waypoint(function(direction) {
-     if(direction == "down") $('.navbar-wrapper').addClass("scrolled");
-     if(direction == "up") $('.navbar-wrapper').removeClass("scrolled");
-    }, {
-      offset: -50 // When the bottom of the element is fully above the bottom of the viewport
-    })
-    
-    $('.btn.btn-navbar').click( function() {
-    
-    	if($(this).hasClass("collapsed") && !$('.navbar-wrapper').hasClass("scrolled")) {
-    		$('.navbar-wrapper').addClass("scrolled");
-    	}
-    })
     
    
    function scrollToAnchor(aid){
@@ -138,78 +124,6 @@ $(document).ready(function() {
    }
    
   
-   $('.navbar li a').click(function(event){
-	if ($(this).attr("href")[0] == "#"){
-           event.preventDefault();
-           //calculate destination place
-            $('.navbar li a').parent().removeClass("active");
-           $(this).parent().addClass("active");
-           var dest=0;
-           if($(this.hash).offset().top > $(document).height()-$(window).height()){
-                dest=$(document).height()-$(window).height();
-           }else{
-                dest=$(this.hash).offset().top -100;
-           }
-           //go to destination
-           $('html,body').animate({scrollTop:dest}, 1000);
-           // close menu if necessary
-           if($('.navbar-collapse').hasClass('in')) { $('.btn-navbar').click(); }
-		}
-       });  
-       
-     $('.contact-btn').click(function(event){
-             event.preventDefault();
-             //calculate destination place
-             $('.navbar li a').parent().removeClass("active");
-             $('.navbar li:last-child').addClass("active");
-             var dest=0;
-             if($(this.hash).offset().top > $(document).height()-$(window).height()){
-                  dest=$(document).height()-$(window).height();
-             }else{
-                  dest=$(this.hash).offset().top -100;
-             }
-             //go to destination
-             $('html,body').animate({scrollTop:dest}, 1000);
-         });    
-            
-            
-     
-     // Contact form
-     	$(".contact-form").submit(function() {
-    
-     	var this_form = $(this);
-     	$.ajax({
-     		type: 'post',
-     		data: this_form.serialize(),
-     		url: 'send_email.php',
-     		success: function(res) {
-     			if(res == "true") {
-     				//this_form.fadeOut("fast");
-     					$(".success").fadeIn("fast");
-     					$(".validation").fadeOut("fast");
-     			} else {
-     				$(".validation").fadeIn("fast");
-     				this_form.find(".text").removeClass("error");
-     				$.each(res.split(","), function() {
-     					this_form.find("#"+this).addClass("error");
-     				});
-     			}
-     		}
-     	});
-     });
-     
-     
-     /* VIDEO AUTOPLAY AUTOSTOP */
-     
-     $('#inline-video').on('show', function () {
-       $('#inline-video div.modal-body #vidholder').html('<iframe src="http://www.youtube.com/v/RPpV_dELuBw&amp;rel=0&amp;autohide=1&amp;showinfo=0&amp;autoplay=1" width="500" height="281" frameborder="0" allowfullscreen=""></iframe>'); 
-       
-       
-     });
-     
-     $('#inline-video').on('hide', function () {
-       $('#inline-video div.modal-body #vidholder').html('&nbsp;');  
-     });
      
 
 });
