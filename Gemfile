5f84c7e7223fa81fdf1f9f545c2725256dbc31ed
Franco Sebregondi <me@usershell.org>
2015-10-19 10:41:18 -1000

    Replace 2014 website with fresh middleman page

--- Gemfile
+++ Gemfile
@@ -1,14 +1,15 @@
-# If you have OpenSSL installed, we recommend updating
-# the following line to use "https"
-source 'http://rubygems.org'
-
-gem "middleman", "~>3.3.4"
-
-gem "middleman-livereload", "~> 3.1.0"
-gem "middleman-deploy", "~> 0.3.0"
+# If you do not have OpenSSL installed, change
+# the following line to use 'http://'
+source 'https://rubygems.org'
 
 # For faster file watcher updates on Windows:
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
 
 # Windows does not come with time zone data
-gem "tzinfo-data", platforms: [:mswin, :mingw]
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+
+# Middleman Gems
+gem 'middleman-sprockets', '>= 4.0.0.rc.1'
+gem 'middleman-compass', '>= 4.0.0.rc.1'
+gem 'middleman', '>= 4.0.0.rc.1'
+gem 'middleman-livereload'
--- config.rb
+++ config.rb
@@ -1,44 +1,29 @@
 ###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
 # Page options, layouts, aliases and proxies
 ###
 
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
-#
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+# page "/path/to/file.html", layout: :otherlayout
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
 ###
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
 # Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
+# configure :development do
+#   activate :livereload
+# end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -47,46 +32,11 @@ end
 #   end
 # end
 
-set :css_dir, 'css'
-
-set :js_dir, 'js'
-
-set :images_dir, 'img'
-
-set :relative_links, true
-
 # Build-specific configuration
 configure :build do
-  # Minify css on build
-  activate :minify_css
+  # Minify CSS on build
+  # activate :minify_css
 
   # Minify Javascript on build
-  activate :minify_javascript
-
-  # Enable cache buster
-  activate :asset_hash
-
-  # Use relative URLs
-  activate :relative_assets
-
-  # svg's are inlined in the html pages
-  ignore 'img/inline_svgs/*.svg'
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
-end
-
-# Deployment configuration
-activate :deploy do |deploy|
-  deploy.build_before = true
-
-  deploy.method = :ftp
-
-  deploy_config = YAML.load(File.read 'deploy.yml') rescue nil
-  if deploy_config.kind_of? Hash
-    deploy.host     = deploy_config['host']
-    deploy.path     = deploy_config['path']
-    deploy.user     = deploy_config['user']
-    deploy.password = deploy_config['password']
-  end
+  # activate :minify_javascript
 end
