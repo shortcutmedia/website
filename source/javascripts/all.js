7bf04275696de41ee4c2969600d66139ccbb7c15
Franco Sebregondi <me@usershell.org>
2015-11-27 10:54:09 -1000

    optimizations

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,19 @@
 //= require jquery
-//= require bootstrap-sprockets
+
+
+// Instead of require entire bootstrap-sprockets only import
+// sellected files
+//= require bootstrap/affix
+//= require bootstrap/alert
+//= require bootstrap/button
+// require bootstrap/carousel
+//= require bootstrap/collapse
+//= require bootstrap/dropdown
+// require bootstrap/tab
+//= require bootstrap/transition
+//= require bootstrap/scrollspy
+// require bootstrap/modal
+// require bootstrap/tooltip
+// require bootstrap/popover
+
 //= require_tree .
